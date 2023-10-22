f676e9f8505ab20ae75c5bfcb76b6abf73433b16
Usman Jamil Abbasi <usmanabbasi123678@gmail.com>
2023-10-22 06:58:19 -0700

    finding radian

--- airDistance.c
+++ airDistance.c
@@ -4,3 +4,10 @@
  double lon1,p1,lon2,p2,delta,Radius = 62371,Result=0;
 //Function to convert Degree into Radian
 int Degree_to_Radian(int num)
+{
+    float PI = 3.1416f;
+    int radian = (num/180)*PI;
+
+    return radian;
+}
+
